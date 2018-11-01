e6d0a9600286ed50dccee50f4e68ee473703b52d
Carl Richmond <ACC25@users.noreply.github.com>
2018-11-01 11:42:32 -0700

    adds mean test

--- common/mean.ts
+++ common/mean.ts
@@ -2,7 +2,7 @@ import { Scale } from "./interfaces/scale_enum";
 import { round } from "./round";
 
 const mean = (list: number[], scale: Scale): number => {
-    let sum: number;
+    let sum: number = 0;
     for (const element of list) {
         sum += element;
     }
--- common/test/round.test.ts
+++ common/test/round.test.ts
@@ -21,7 +21,27 @@ test("it rounds to the nearest hundredths", () => {
     expect(round(5, Scale.HUNDREDTHS)).toBe(5);
 });
 
+test("it rounds to the nearest thousandths", () => {
+    expect(round(4.3566, Scale.THOUSANDTHS)).toBe(4.357);
+    expect(round(4.5, Scale.THOUSANDTHS)).toBe(4.5);
+    expect(round(4.8889, Scale.THOUSANDTHS)).toBe(4.889);
+    expect(round(5.016, Scale.THOUSANDTHS)).toBe(5.016);
+    expect(round(4.300009, Scale.THOUSANDTHS)).toBe(4.3);
+    expect(round(-4.3566, Scale.THOUSANDTHS)).toBe(-4.357);
+    expect(round(5, Scale.THOUSANDTHS)).toBe(5);
+});
+
 test("it rounds to the nearest ten hundredths", () => {
+    expect(round(4.35667, Scale.TEN_THOUSANDTHS)).toBe(4.3567);
+    expect(round(4.5, Scale.TEN_THOUSANDTHS)).toBe(4.5);
+    expect(round(4.88899, Scale.TEN_THOUSANDTHS)).toBe(4.889);
+    expect(round(5.01677, Scale.TEN_THOUSANDTHS)).toBe(5.0168);
+    expect(round(4.300009, Scale.TEN_THOUSANDTHS)).toBe(4.3000);
+    expect(round(-4.35664, Scale.TEN_THOUSANDTHS)).toBe(-4.3566);
+    expect(round(5, Scale.TEN_THOUSANDTHS)).toBe(5);
+});
+
+test("it rounds to the nearest hundred thousandths", () => {
     expect(round(4.35667, Scale.TEN_THOUSANDTHS)).toBe(4.3567);
     expect(round(4.5, Scale.TEN_THOUSANDTHS)).toBe(4.5);
     expect(round(4.88899, Scale.TEN_THOUSANDTHS)).toBe(4.889);
